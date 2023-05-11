670a932726a8159f138b7bdedf602414614ab9cc
Conor Panuccio <conorpanu@hotmail.com>
2023-05-10 20:27:18 -0400

    execpt_bonus

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
+function move(element){
+    element.style.position = 'fixed'
+    
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+    function moveToCoordinates(left, bottom){
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }return{
+        to: moveToCoordinates
+    }
+}
 
+
+move(newImage('assets/green-character.gif')).to(100, 250)
+
+
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
-    item.addEventListener('click', () => {
+    item.addEventListener('dblclick', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
         inventoryItem.src = url;
@@ -27,9 +37,9 @@ function newItem(url, left, bottom){
     return item
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
 
 function newInventory(){
     let inventory = document.createElement('div')
@@ -47,5 +57,6 @@ function newInventory(){
     document.body.append(inventory)
     return inventory
 }
+
 
 const inventory = newInventory()
